f5e8344e6a381c3bc8056e992c8f88debe28e233
zhengwei6 <40619175+zhengwei6@users.noreply.github.com>
2019-09-21 12:00:39 +0800

    Add files via upload

--- ESP32_Custom_Vision/ESP32_Custom_Vision/custom_vision_send.ino
+++ ESP32_Custom_Vision/ESP32_Custom_Vision/custom_vision_send.ino
@@ -1,81 +1,58 @@
-void custom_vision_send(String filename , camera_fb_t * fb, String Sendhost, String boundary, String Sendurl, String key){
+/***********************************************************************
+ *  ESP32 Custom Vision function
+ *
+ *  Author : Zheng Wei Wu(2019/09/21)
+ * 
+ *  input: 
+ *        String filename : SD card file name
+ *        String Sendhost : Custom Vision Server
+ *        String Sendurl  : url of your project
+ *        String key      : key of your project
+ *  output : 
+ *        String response
+ ***********************************************************************/
+String custom_vision_send(String filename, String Sendhost, String Sendurl, String key){
+  String response = "";
   // Create postheader,keyheader,bodyheader
-  String postheader,keyheader,bodyheader,bodyend;
-  Serial.println("Preparing key header");
-  keyheader  = keyheader_create(boundary);
-  Serial.println("Preparing body header");
-  bodyheader = bodyheader_create(boundary, filename);
-  Serial.println("Preparing body end");
-  bodyend    = bodyend_create(boundary);
-  
-  size_t allLen = keyheader.length() + fb->len + bodyheader.length() + bodyend.length();
-  Serial.println("Preparing post header");
-  postheader = postheader_create(boundary, Sendhost, Sendurl, allLen, key);
-  Serial.println("Code: ");
-  Serial.print(postheader + keyheader + bodyheader + bodyend);
-  
-  Serial.println("Starting connection to server");
-  Serial.println(Sendhost);
-  WiFiClientSecure client;
-  if (!client.connect(SERVER,443)) {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  HTTPClient https;
+  Serial.print("[HTTPS] begin...\n");
+
+  // set up SSL client
+  if (!client->connect(SERVER,443)) {
     Serial.println("connection failed");
-    return;   
+    return response;   
+  }else{
+    Serial.println("Client connect success!");
   }
-  client.print(postheader + keyheader + bodyheader);
-  // client write
-  client.write(fb->buf, fb->len);
-  client.print(bodyend);
-  delay(20);
-  long tOut = millis() + TIMEOUT;
-  Serial.print("waiting");
-  while(client.connected() && tOut > millis()) {
-    Serial.print("...");
-    if (client.available()) 
-    {
-      String serverRes = client.readStringUntil('\r');
-      Serial.print(serverRes);
-      return;
-    }
+
+  // set up https and send file
+  if (https.begin(*client,"https://westus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/58a8698a-abfb-4732-bfbb-ef5199b4ed0b/detect/iterations/Iteration1/image")) {
+     Serial.println("begin sucess");
+     https.addHeader("Accept", "application/json");
+     https.addHeader("Content-Type", "application/octet-stream");
+     https.addHeader("Prediction-Key", key);
+     fs::FS &fs = SD_MMC; 
+     File file = fs.open(filename, "r");
+     https.sendRequest("POST", &file, file.size());
+     delay(20);
+     long tOut = millis() + TIMEOUT;
+     Serial.print("waiting");
+     while(client->connected() && tOut > millis()) {
+       Serial.print("...");
+       if (client->available()) 
+       {
+          response = client->readStringUntil('\r');
+          Serial.print(response);
+           https.end();
+           file.close();
+        }
+     }
+     https.end();
+     file.close();
+  }else{
+    Serial.println("http client begin error");
   }
-  return;
-}
-
-String postheader_create(String boundary, String Sendhost, String Sendurl,size_t length, String key){
-  String postheader;
-  postheader = "POST " + Sendurl + " HTTP/1.1\r\n";
-  postheader += "Host: " + Sendhost + "\r\n";
-  postheader += "Prediction-Key: " + key + "\r\n";
-  postheader += "Content-Type: multipart/form-data; boundary=----" + boundary + "\r\n";
-  postheader += "Accept: */*\r\n";
-  postheader += "Connection: keep-alive\r\n";
-  postheader += "content-length: ";
-  postheader += String(length);
-  postheader += "\r\n";
-  postheader += "\r\n";
-  return postheader;
-}
-
-String keyheader_create(String boundary){
-  String keyheader;
-  keyheader = "------";
-  keyheader += boundary;
-  keyheader += "\r\n";
-  keyheader += "Content-Disposition: form-data; name=\"key\"\r\n\r\n";
-  return (keyheader);
-}
-
-String bodyheader_create(String boundary,String filename){
-  String bodyheader;
-  bodyheader = "------";
-  bodyheader += boundary;
-  bodyheader += "\r\n";
-  bodyheader += "Content-Disposition: form-data; name=\"imageFile\"; filename=\"" + filename + "\"\r\n";
-  bodyheader += "Content-Type: Content-Type: image/jpg\r\n";
-  bodyheader += "\r\n";
-  bodyheader += "\r\n";
-  return(bodyheader);
-}
-String bodyend_create(String boundary){
-  String bodyEnd =  "------"+boundary+"--\r\n";
-  return(bodyEnd);
+  
+  return(response);
 }
